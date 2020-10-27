ca432cf7584ccfbef347d3ad2c5259276db2a9a5
Денис Вилков <d.vilkov@kvantorium62.local>
2020-10-27 19:54:46 +0300

    feat: add upgrade

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let points = 0;
-let pointsPerClick = 1;
+let pointsPerTick = 1;
+let upgradePrice = 50;
+let upgradeBtn = document.getElementById("upgrade-ppt-btn");
+let timeInterval = 1000;
 
 function keyPressHandler(event) {
   if (event.code === "Space") {
@@ -8,13 +11,22 @@ function keyPressHandler(event) {
 }
 
 function increasePoints() {
-  //points = points + pointsPerClick;
-  points += pointsPerClick;
-
-  // let element = document.getElementById("points");
-  // element.innerText = points;
+  points += pointsPerTick;
   document.getElementById("points").innerText = points;
 }
 
-document.addEventListener("keypress", keyPressHandler);
-setInterval(increasePoints, 1000);
+function upgradePointsPerTick() {
+  if (points >= upgradePrice) {
+    points -= upgradePrice;
+    upgradePrice *= 2;
+    ++pointsPerTick;
+    document.getElementById("upgrade-ppt-price").innerText = upgradePrice;
+    document.getElementById("points-per-second").innerText =
+      (1000 * pointsPerTick) / timeInterval;
+    document.getElementById("points").innerText = points;
+  }
+}
+
+document.onkeypress = keyPressHandler;
+upgradeBtn.onclick = upgradePointsPerTick;
+let interval = setInterval(increasePoints, 1000);
